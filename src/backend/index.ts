e2ecd0fcc0573213cc90ee2de5fd00176a0b52ea
Vladislav Komissarov <mastyf@gmail.com>
2022-04-27 08:24:04 +0300

    fix for build

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,11 +1,12 @@
 import "./pre-start/index"; // Must be the first import
 import app from "./Server";
 import logger from "./shared/Logger";
+import ConfigManager from "./shared/ConfigManager";
 
 // Start the server
 const port = Number(process.env.PORT || 8080);
 app.listen(port, () => {
   logger.info("Express server started on port: " + port);
-  logger.info("Var test: " + process.env.TELEGRAM_BOT_TOKEN);
+  logger.info("Var test: " + ConfigManager.get("TELEGRAM_BOT_TOKEN"));
 });
 
